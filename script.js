9bf57d2da618a454fe8f54dee935c6cb80ed683d
dubideployer <bitomaszcyganek@gmail.com>
2024-11-29 00:11:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   }
 </script>
+<script>
+  document.querySelector('.dropdown').addEventListener('click', function(event) {
+    var dropdownMenu = this.querySelector('.dropdown-content');
+    dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
+    event.stopPropagation();
+  });
+
+  // Zamknięcie menu, gdy użytkownik kliknie gdziekolwiek poza menu
+  document.addEventListener('click', function(event) {
+    var dropdowns = document.querySelectorAll('.dropdown-content');
+    dropdowns.forEach(function(menu) {
+      if (!menu.contains(event.target)) {
+        menu.style.display = 'none';
+      }
+    });
+  });
+</script>
